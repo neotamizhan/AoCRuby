01e4c3eff715ce8ce8b4489ff88d47e440f95c43
Siddharth Venkatesan <neotamizhan@gmail.com>
2021-12-01 10:23:34 +0000

    optimized a bit

--- 2021/day-01/day-01.rb
+++ 2021/day-01/day-01.rb
@@ -1,18 +1,12 @@
 def load
   file_path = File.expand_path("../day-01.txt", __FILE__)
-  lines = File.readlines(file_path)
-  depths = lines.map(&:to_i)
-
-  depths
+  File.readlines(file_path).map(&:to_i)
 end
 
 def process_part1
   depths = load
   deeper = 0
-  depths.each_with_index do |v,i|
-    next if i==0 
-    deeper = deeper + 1 if depths[i] > depths[i-1]
-  end
+  depths.each_with_index { |v,i| deeper += 1 if i > 0 and depths[i] > depths[i-1] }  
 
   puts deeper
 end
@@ -22,14 +16,9 @@ def process
   depths = load
   deeper = 0
   sliding_sum = []
-  depths.each_with_index do |v,i|
-    sliding_sum << depths[i] + depths[i+1] + depths[i+2] if depths[i+2]
-  end
+  depths.each_with_index { |v,i| sliding_sum << depths[i] + depths[i+1] + depths[i+2] if depths[i+2] }
 
-  sliding_sum.each_with_index do |v,i|
-    next if i==0 
-    deeper = deeper + 1 if sliding_sum[i] > sliding_sum[i-1]
-  end
+  sliding_sum.each_with_index { |v,i| deeper += 1 if sliding_sum[i] > sliding_sum[i-1] }  
   
   puts deeper
 end
