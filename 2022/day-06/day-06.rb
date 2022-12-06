4d25d83f1e788f10651d90017b96b2253f128441
Siddharth Venkatesan <neotamizhan@gmail.com>
2022-12-06 11:40:01 +0000

    completed 2022 day 6

--- 2022/day-06/day-06.rb
+++ 2022/day-06/day-06.rb
@@ -1,19 +1,33 @@
+def get_marker(line, len)
+  start = 0; has_marker = false
+  until has_marker do
+    has_marker = isSOP?(line[start..start+len-1])
+    start = start + 1 unless has_marker
+  end
+  
+  start+len
+end
+
+#is packet start?
+def isSOP?(str)  
+  str.size == str.split('').uniq.size
+end
+
 def load (is_test: true)      
   file_path =  File.expand_path("../data#{is_test ? '-sample' : ''}.txt", __FILE__) 
-  lines = File.readlines(file_path)
-
+  line = File.read(file_path)  
 end
 
 
 def process_part1
-  load
+  puts get_marker(load(is_test: false), 4)  
 end
 
 
 def process_part2  
-  
+  puts get_marker(load(is_test: false), 14)  
 end
 
 def process
-  process_part1
+  process_part2
 end
